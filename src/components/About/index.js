1d9c62de435b58c3c6bf994f8f7ac52b9bf999fb
lucas leiro <lucasleiroa@gmail.com>
2024-06-05 15:11:00 -0300

    sidebar responsive

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
 import "../../App.css";
 import DownloadIcon from "@mui/icons-material/Download";
-// import * as LottiePlayer from "@lottiefiles/lottie-player";
+import * as LottiePlayer from "@lottiefiles/lottie-player";
 
 export default function About() {
   const getDate = () => {
@@ -15,66 +15,68 @@ export default function About() {
   return (
     <div
       id="about"
-      className="min-h-screen bg-white flex flex-col text-center gap-5 text-black text-sm lg:text-lg  font-normal"
+      className="min-h-screen bg-white flex flex-col justify-center items-center gap-5 text-black text-sm lg:text-lg  font-normal p-4"
     >
       <div
-        className="head text-3xl lg:text-5xl mt-12 font-bold"
+        className="head text-3xl lg:text-5xl font-bold text-center"
         data-aos={"slide-down"}
       >
         About Me
       </div>
-      <div className="flex flex-row  gap-6 ml-8">
-        <div className="flex-col mt-14 flex flex-auto w-64 gap-6">
-          <h3
-            className="text-xl lg:text-3xl font-medium"
-            data-aos={"fade-left"}
-          >
+      <div className="flex flex-col items-center gap-6">
+        <div className="flex-col gap-6">
+          <h3 className="text-xl lg:text-3xl font-medium text-center">
             I'm <span className="text-yellow-600">Lucas Leiro,</span> a Web
             Enthusiast
           </h3>
-          <p
-            className="pt-5 leading-7 text-slate-900 text-justify"
-            data-aos={"fade-left"}
-          >
-            I love playing football, ride a bike and do sports. I graduated from the National University of Tucumán (UTN).
-             I am {getDate()}{" "}
-            years old.I love exploring new technologies.
+          <p className="leading-7 text-slate-900 text-justify">
+            I love playing football, ride a bike and do sports. I graduated
+            from the National University of Tucumán (UTN). I am {getDate()}{" "}
+            years old. I love exploring new technologies.
           </p>
-          <p
-            className="text-justify leading-7 text-slate-900"
-            data-aos={"fade-right"}
-          >
-          I am currently looking for work. I am also a web developer.
-           I worked on reactJs, Express, TypeScript, Angular and many more. 
-           I am also looking to collaborate on open source projects.
+          <p className="leading-7 text-slate-900 text-justify">
+            I am a passionate programmer, holding a degree as a programming
+            technician from the Universidad Tecnológica Nacional (UTN) in
+            Tucumán, Argentina. Currently, I am part of the development team
+            at Fina Partner, where I have had the privilege of gaining valuable
+            experience in the field of software development and teamwork. I am
+            a practical and meticulous person, focused on achieving excellence
+            from the outset and always seeking ways to optimize and improve our
+            processes to achieve even better results. My focus is on efficiency
+            and quality. I firmly believe in the importance of creating
+            solutions that are not only effective but also innovative and
+            creative. That's why I greatly enjoy creating applications and
+            developing solutions that make a difference. I am actively seeking
+            a new job opportunity that will allow me to continue growing
+            professionally and apply my skills in a dynamic and challenging
+            environment. I am confident that I can add significant value to
+            your team with my experience, dedication, and creative mindset. I
+            sincerely appreciate your time and consideration. I am available
+            for an interview at your convenience. I look forward to the
+            possibility of working together and creating something
+            extraordinary!
           </p>
         </div>
 
-        <div
-          class="relative flex-auto w-32   sm:rounded-lg  pl-4"
-          data-aos={"slide-left"}
-        >
-          {/* <img
-            src={LearningCode}
-            alt="Learning Code"
-            className="motion-safe:animate-zoomy"
-          /> */}
+        <div className="w-full lg:w-2/3">
           <div className="motion-safe:animate-zoomy">
             <lottie-player
               autoplay
               loop
               mode="bounce"
               src="https://assets10.lottiefiles.com/packages/lf20_w98qte06.json"
-              style={{ width: "350px" }}
+              style={{ width: "100%" }}
             />
           </div>
+        </div>
 
+        <div className="w-full flex justify-center">
           <a
             href={require("../../assets/files/Lucas Resume.pdf")}
             download={"Lucas Resume"}
             target="_blank"
             rel="noreferrer"
-            className="py-1 md:py-2 lg:py-3 mt-3 lg:mt-5 text-white mx-auto px-4  lg:px-9 bg-blue-600 border-2 w-fit border-blue-500 rounded-3xl  hover:-translate-y-1.5 duration-[350ms] hover:duration-[350ms] hover:bg-blue-800 hover:scale-[1.023] focus:bg-blue-800 sm:text-xl  animate-zoomy"
+            className="py-2 md:py-3 lg:py-4 mt-3 lg:mt-5 text-white mx-auto px-6 lg:px-10 bg-blue-600 border-2 w-fit border-blue-500 rounded-3xl hover:-translate-y-1.5 duration-[350ms] hover:duration-[350ms] hover:bg-blue-800 hover:scale-[1.023] focus:bg-blue-800 sm:text-xl animate-zoomy"
           >
             Download CV
             <DownloadIcon />
--- src/components/SlideBar.js
+++ src/components/SlideBar.js
@@ -3,11 +3,14 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Slidebar() {
   const [select, setSelect] = useState(0);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
   const scroll = (section) => {
     if (section === "#") {
@@ -20,151 +23,179 @@ export default function Slidebar() {
     }
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="flex-none hidden lg:block  bg-black h-screen min-w-[25%] fixed ">
-      <div className="nav flex  text-white text-lg mt-10 flex-col align-middle justify-center text-center w-full gap-5 overflow-hidden">
-        <div data-aos="slide-down">
-          <NavLink to={"/activity"}>
-            <img
-              src={require("../assets/images/MyPerfil.jpeg")}
-              alt="Daniel Jebarson"
-              title="Discord Activity"
-              className="rounded-full border-solid cursor-pointer  border-[8px] border-stone-600 min-h-fit mx-auto  max-w-[190px]"
-            />
-            <h3 className="text-white name py-4 font-medium ">
-              Lucas Leiro
-            </h3>
-          </NavLink>
+    <>
+      <div className="fixed top-0 w-full flex justify-between items-center bg-black text-white p-4 z-50">
+        <button onClick={toggleSidebar}>
+          {isSidebarOpen ? <CloseIcon /> : <MenuIcon />}
+        </button>
+        <div className="ml-auto">
+          <img
+            src={require("../assets/images/exploiter.webp")}
+            alt="Logo"
+            className="h-10"
+          />
         </div>
-        <p
-          onClick={() => setSelect(0)}
-          className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
-            select === 0 ? "text-blue-600 " : ""
-          }`}
-          data-aos="slide-right"
-          data-aos-delay="200"
-        >
-          <NavLink
-            to="#"
-            onClick={() => {
-              scroll("#");
-            }}
-          >
-            <p title="Home Page"> Home</p>
-          </NavLink>
-        </p>
-        <p
-          onClick={() => setSelect(1)}
-          className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
-            select === 1 ? "text-blue-600 " : ""
-          }`}
-          data-aos-delay="600"
-          data-aos="slide-left"
-        >
-          <NavLink
-            to="#about"
-            onClick={() => {
-              scroll("about");
-            }}
-          >
-            <p title="About me"> About Me</p>
-          </NavLink>
-        </p>
-        <p
-          onClick={() => setSelect(2)}
-          className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
-            select === 2 ? "text-blue-600 " : ""
-          }`}
-          data-aos="slide-right"
-          data-aos-delay="1000"
-        >
-          <NavLink
-            to="#resume"
-            onClick={() => {
-              scroll("resume");
-            }}
-          >
-            <p title="Resume"> Resume</p>
-          </NavLink>
-        </p>
-        <p
-          onClick={() => setSelect(3)}
-          className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
-            select === 3 ? "text-blue-600 " : ""
-          }`}
-          data-aos="slide-left"
-          data-aos-delay="1400"
-        >
-          <NavLink
-            to="#projects"
-            onClick={() => {
-              scroll("projects");
-            }}
-          >
-            <p title="Projects"> Projects</p>
-          </NavLink>
-        </p>
-        <p
-          onClick={() => setSelect(4)}
-          className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
-            select === 4 ? "text-blue-600 " : ""
-          }`}
-          data-aos="slide-right"
-          data-aos-delay="1800"
-        >
-          <NavLink
-            to="#contact"
-            onClick={() => {
-              scroll("contact");
-            }}
-          >
-            <p title="Contact"> Contact</p>
-          </NavLink>
-        </p>
       </div>
+
       <div
-        className="text-white flex flex-row gap-5 w-fit mx-auto pt-7"
-        data-aos="slide-up"
-        data-aos-delay="2300"
+        className={`flex-none lg:block bg-black h-screen fixed z-40 transition-transform duration-300 ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
+        } w-full lg:w-[25%]`}
       >
-        <div
-          title="Github"
-          onClick={() =>
-            window.open("https://github.com/daniel-jebarson", "_blank")
-          }
-        >
-          <GitHubIcon className="cursor-pointer hover:scale-105" />
+        <div className="nav flex text-white text-lg mt-10 flex-col align-middle justify-center text-center w-full gap-5 overflow-hidden pt-10">
+          <div data-aos="slide-down">
+            <NavLink to={"/activity"}>
+              <img
+                src={require("../assets/images/MyPerfil.jpeg")}
+                alt="Lucas Leiro"
+                title="Discord Activity"
+                className="rounded-full border-solid cursor-pointer border-[8px] border-stone-600 min-h-fit mx-auto max-w-[190px]"
+              />
+              <h3 className="text-white name py-4 font-medium">
+                Lucas Leiro
+              </h3>
+            </NavLink>
+          </div>
+          <p
+            onClick={() => setSelect(0)}
+            className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
+              select === 0 ? "text-blue-600 " : ""
+            }`}
+            data-aos="slide-right"
+            data-aos-delay="200"
+          >
+            <NavLink
+              to="#"
+              onClick={() => {
+                scroll("#");
+                toggleSidebar();
+              }}
+            >
+              <p title="Home Page"> Home</p>
+            </NavLink>
+          </p>
+          <p
+            onClick={() => setSelect(1)}
+            className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
+              select === 1 ? "text-blue-600 " : ""
+            }`}
+            data-aos-delay="600"
+            data-aos="slide-left"
+          >
+            <NavLink
+              to="#about"
+              onClick={() => {
+                scroll("about");
+                toggleSidebar();
+              }}
+            >
+              <p title="About me"> About Me</p>
+            </NavLink>
+          </p>
+          <p
+            onClick={() => setSelect(2)}
+            className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
+              select === 2 ? "text-blue-600 " : ""
+            }`}
+            data-aos="slide-right"
+            data-aos-delay="1000"
+          >
+            <NavLink
+              to="#resume"
+              onClick={() => {
+                scroll("resume");
+                toggleSidebar();
+              }}
+            >
+              <p title="Resume"> Resume</p>
+            </NavLink>
+          </p>
+          <p
+            onClick={() => setSelect(3)}
+            className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
+              select === 3 ? "text-blue-600 " : ""
+            }`}
+            data-aos="slide-left"
+            data-aos-delay="1400"
+          >
+            <NavLink
+              to="#projects"
+              onClick={() => {
+                scroll("projects");
+                toggleSidebar();
+              }}
+            >
+              <p title="Projects"> Projects</p>
+            </NavLink>
+          </p>
+          <p
+            onClick={() => setSelect(4)}
+            className={`cursor-pointer hover:text-blue-600 hover:-translate-y-0.5 hover:text-xl transition hover:transition ${
+              select === 4 ? "text-blue-600 " : ""
+            }`}
+            data-aos="slide-right"
+            data-aos-delay="1800"
+          >
+            <NavLink
+              to="#contact"
+              onClick={() => {
+                scroll("contact");
+                toggleSidebar();
+              }}
+            >
+              <p title="Contact"> Contact</p>
+            </NavLink>
+          </p>
         </div>
         <div
-          title="Instagram"
-          onClick={() =>
-            window.open("https://www.instagram.com/daniel_diago2003", "_blank")
-          }
+          className="text-white flex flex-row gap-5 w-fit mx-auto pt-7"
+          data-aos="slide-up"
+          data-aos-delay="2300"
         >
-          {" "}
-          <InstagramIcon className="cursor-pointer hover:scale-105" />
-        </div>
-        <div
-          title="Linkedin"
-          onClick={() =>
-            window.open(
-              "https://www.linkedin.com/in/daniel-jebarson-k-a727a822a",
-              "_blank"
-            )
-          }
-        >
-          {" "}
-          <LinkedInIcon className="cursor-pointer hover:scale-105" />
-        </div>
-        <div
-          title="Facebook"
-          onClick={() =>
-            window.open("https://www.facebook.com/daniel.jebarson.9", "_blank")
-          }
-        >
-          <FacebookIcon className="cursor-pointer hover:scale-105" />
+          <div
+            title="Github"
+            onClick={() =>
+              window.open("https://github.com/LucasLeiroA", "_blank")
+            }
+          >
+            <GitHubIcon className="cursor-pointer hover:scale-105" />
+          </div>
+          <div
+            title="Instagram"
+            onClick={() =>
+              window.open("https://www.instagram.com/_lucasleiro/", "_blank")
+            }
+          >
+            {" "}
+            <InstagramIcon className="cursor-pointer hover:scale-105" />
+          </div>
+          <div
+            title="Linkedin"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/lucasleiro/",
+                "_blank"
+              )
+            }
+          >
+            {" "}
+            <LinkedInIcon className="cursor-pointer hover:scale-105" />
+          </div>
+          <div
+            title="Facebook"
+            onClick={() =>
+              window.open("https://www.facebook.com/lucas.leiro.12", "_blank")
+            }
+          >
+            <FacebookIcon className="cursor-pointer hover:scale-105" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
